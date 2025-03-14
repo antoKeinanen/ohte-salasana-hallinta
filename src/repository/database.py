da2cc058f4740f5833c41bdcb5838fab05ba8348
AntoKeinanen <anto.keinanen@gmail.com>
2025-03-14 22:09:28 +0200

    Viikko 3: siirrä discover_vaults Database luokan sisään

--- src/repository/database.py
+++ src/repository/database.py
@@ -1,5 +1,9 @@
+from glob import glob
 import os
 import sqlite3
+import platformdirs
+
+from src.constants import APP_NAME
 
 
 class Database:
@@ -16,3 +20,24 @@ class Database:
         if Database.connection:
             Database.connection.close()
             Database.connection = None
+
+    @staticmethod
+    def discover_databases(path: None | os.PathLike):
+        """
+        Discover vault databases in the specified directory.
+
+        If the path is not provided, it defaults to the user data path for the application.
+        If the directory does not exist, it will be created.
+
+        Args:
+            path (None | os.PathLike): The directory path to search for vault databases.
+                                    If None, the default user data path is used.
+
+        Returns:
+            list: A list of paths to the discovered vault databases.
+        """
+        path = path or platformdirs.user_data_path(APP_NAME)
+        if not path.exists():
+            os.makedirs(path)
+        path.joinpath("*.sqlite")
+        return glob(path)
