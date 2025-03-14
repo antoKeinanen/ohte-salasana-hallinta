42af44e5bddf64f6b4ca42062f47fd013f21677f
AntoKeinanen <anto.keinanen@gmail.com>
2025-03-14 22:04:09 +0200

    Viikko 3: lisää 'watch' asetus invoke komentoihin

--- tasks.py
+++ tasks.py
@@ -1,18 +1,28 @@
 from invoke import task
 
+WATCH_COMMAND = "watchmedo shell-command --patterns '*.py' --drop --command='{}' src"
 
-@task
-def start(ctx):
-    ctx.run("python3 src/main.py", pty=True)
+
+@task(optional=["watch"])
+def start(ctx, watch=None):
+    if not watch:
+        return ctx.run("python3 src/main.py", pty=True)
+    print(WATCH_COMMAND.format("invoke start"))
+    ctx.run(WATCH_COMMAND.format("invoke start"), pty=True)
+
 
 @task
 def test(ctx):
     ctx.run("pytest", pty=True)
 
+
 @task
 def coverage(ctx):
     ctx.run("coverage run --branch -m pytest src", pty=True)
 
-@task(coverage)
-def coverage_report(ctx):
-    ctx.run("coverage html", pty=True)
+
+@task(coverage, optional=["watch", "mode"])
+def coverage_report(ctx, watch=None, mode="html"):
+    if not watch:
+        return ctx.run(f"coverage {mode}", pty=True)
+    ctx.run(WATCH_COMMAND.format("invoke coverage-report --mode report"), pty=True)
