72d31ff7c13a7622ee3951a8733d8c0f0e557abc
AntoKeinanen <anto.keinanen@gmail.com>
2025-03-24 15:17:57 +0200

    ref: restart project

--- tasks.py
+++ tasks.py
@@ -1,14 +1,9 @@
 from invoke import task
 
-WATCH_COMMAND = "watchmedo shell-command --patterns '*.py' --drop --command='{}' src"
 
-
-@task(optional=["watch"])
-def start(ctx, watch=None):
-    if not watch:
-        return ctx.run("python3 src/main.py", pty=True)
-    print(WATCH_COMMAND.format("invoke start"))
-    ctx.run(WATCH_COMMAND.format("invoke start"), pty=True)
+@task
+def start(ctx):
+    return ctx.run("python3 src/main.py", pty=True)
 
 
 @task
@@ -21,8 +16,6 @@ def coverage(ctx):
     ctx.run("coverage run --branch -m pytest src", pty=True)
 
 
-@task(coverage, optional=["watch", "mode"])
-def coverage_report(ctx, watch=None, mode="html"):
-    if not watch:
-        return ctx.run(f"coverage {mode}", pty=True)
-    ctx.run(WATCH_COMMAND.format("invoke coverage-report --mode report"), pty=True)
+@task(coverage, optional=["mode"])
+def coverage_report(ctx, mode="html"):
+    return ctx.run(f"coverage {mode}", pty=True)
