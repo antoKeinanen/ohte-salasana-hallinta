edfbaae71f342029f49edf1a8a40cd522c58a937
AntoKeinanen <anto.keinanen@gmail.com>
2025-04-02 12:56:06 +0300

    test: unit tests for credential service

--- src/service/credential_service.py
+++ src/service/credential_service.py
@@ -13,7 +13,7 @@ class CredentialService:
         self.repository = repository
 
     def get_all_credentials(self):
-        credentials = self.repository.get_all_credentials(path=self.vault.path)
+        credentials = self.repository.get_all_credentials(self.vault.path)
         self.vault.credentials = credentials
 
     def get_credential_by_id(self, credential_id: int):
