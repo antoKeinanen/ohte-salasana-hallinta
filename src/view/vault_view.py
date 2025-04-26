2cac024944c6bb4d092a8cb241da0c0d61dfad17
AntoKeinanen <anto.keinanen@gmail.com>
2025-04-26 11:45:14 +0300

    feat: closing the open vault

--- src/view/vault_view.py
+++ src/view/vault_view.py
@@ -18,10 +18,15 @@ class VaultView(Frame):
         self._credentials = self._vault.credentials
 
         self.grid_columnconfigure(1, weight=1)
-        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+
+        self.close_vault_button = Button(
+            self, text="Sulje holvi", command=self._on_close_vault_button_click
+        )
+        self.close_vault_button.grid(row=0, column=0, sticky="nsew")
 
         self.listbox = Listbox(self)
-        self.listbox.grid(row=0, column=0, sticky="ns")
+        self.listbox.grid(row=1, column=0, sticky="ns")
         self.listbox.bind("<<ListboxSelect>>", self._on_listbox_selection_change)
 
         self.create_vault_button = Button(
@@ -29,7 +34,7 @@ class VaultView(Frame):
             text="+ Luo uusi tunnus",
             command=self._on_create_credential_button_click,
         )
-        self.create_vault_button.grid(row=1, column=0, sticky="nsew")
+        self.create_vault_button.grid(row=2, column=0, sticky="nsew")
 
         self.right_container = Frame(self)
         self.right_container.grid(row=0, column=1, rowspan=2)
@@ -71,7 +76,9 @@ class VaultView(Frame):
         self.delete_button.grid()
 
         self.update_button = Button(
-            self.button_row, text="Muokkaa", command=self._on_update_button_click
+            self.button_row,
+            text="Muokkaa",
+            command=self._on_update_credential_button_click,
         )
         self.update_button.grid()
 
@@ -117,7 +124,11 @@ class VaultView(Frame):
     def _on_create_credential_button_click(self):
         self._view_controller.swap_view("create-credential")
 
-    def _on_update_button_click(self):
+    def _on_update_credential_button_click(self):
         self._view_controller.swap_view(
             "update-credential", credential=self._selected_credential
         )
+
+    def _on_close_vault_button_click(self):
+        self._view_controller.app_controller.active_vault = None
+        self._view_controller.swap_view("locked")
