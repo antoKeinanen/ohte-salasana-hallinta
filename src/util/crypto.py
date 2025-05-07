4a787fb27c6d987a9c9d5d2004376ea50db02a83
AntoKeinanen <anto.keinanen@gmail.com>
2025-05-07 16:09:12 +0300

    docs: document crypto module

--- src/util/crypto.py
+++ src/util/crypto.py
@@ -5,6 +5,13 @@ from Crypto.Util.Padding import pad, unpad
 
 
 def encrypt(data: str, key: str) -> str:
+    """
+    Salaa annetun datan AES-algoritmilla
+
+    Args:
+        data: salattava tieto
+        key: salasana, jolla salaus voidaan purkaa
+    """
     derived_key = hashlib.sha256(key.encode()).digest()
     iv = os.urandom(16)
 
@@ -17,6 +24,13 @@ def encrypt(data: str, key: str) -> str:
 
 
 def decrypt(encrypted_data: str, key: str) -> str:
+    """
+    Purkaa annetun datan AES-algoritmilla
+
+    Args:
+        data: tieto. jonka salaus halutaan purkaa
+        key: salasana, jolla salaus voidaan purkaa
+    """
     encrypted_bytes = bytes.fromhex(encrypted_data)
     derived_key = hashlib.sha256(key.encode()).digest()
     iv = encrypted_bytes[:16]
